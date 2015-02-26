75aa126afe909ea2c3c954ac86665715ee55128b
alec resnick <aresnickety@gmail.com>
2015-02-26 13:53:15 -0500

    First commit of GitHub Pages deployment of Letterpress clone's first steps

---
+++ cell.js
@@ -0,0 +1,45 @@
+var Cell = function() {
+    var me = this; // 'this' can point to many, different things, so we grab an easy reference to the object
+    // You can read more about 'this' at:
+    // MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
+    // at http://www.quirksmode.org/js/this.html
+    // and in a more detailed tutorial: http://javascriptissexy.com/understand-javascripts-this-with-clarity-and-master-it/
+
+    // We're going to want to know what row and column I'm in, but we won't 'til I'm added to a board
+    this.row = null;
+    this.column = null;
+
+    // Let's make the HTML that'll display me
+    me.html = document.createElement('div');
+    me.html.classList.add('cell');
+
+    // Let's create an event for when I want to submit myself to the tray
+    // You can read more about this on MDN at https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events
+    var submit = new CustomEvent('cellSubmit', {
+        detail: {
+            'cell': me // and send along all my info
+        }
+    });
+    me.html.addEventListener('click', function(event) {
+    	me.html.dispatchEvent(submit); // and trigger my custom event when someone clicks my HTML
+    });
+
+    var _letter; // the letter I actually contain
+    Object.defineProperty(this, 'letter', {
+        // Custom getters and setters; you can read more about this at:
+        // MDN: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+        // Yehuda Katz's tutorial: http://yehudakatz.com/2011/08/12/understanding-prototypes-in-javascript/
+        get: function() {
+            // when someone asks for cell.letter, just give them back _letter
+            return _letter;
+        },
+        set: function(letter) {
+            // when someone sets cell.letter = something, reset _letter
+            _letter = letter;
+            // and change what my HTML displays
+            me.html.innerHTML = letter;
+        }
+    });
+
+    return this;
+};
---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function() { // When everything is loaded
+    var board = new Board([]); // Create an empty board
+
+    var cells = [];
+    for (var i = 0; i < 5*5; i++) {
+        var cell = new Cell();
+        cell.letter = 'L';
+        cells.push(cell);
+    }; // Make an array of 5*5 = 25 new Cell objects
+
+    board.cells = cells; // and load the cells into the board
+
+    document.body.appendChild(board.html); // Then actually put the Board's HTML into the document
+});
