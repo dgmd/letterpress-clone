73f68d0e3ec45d5a942df9d58856f8a0bf7a0090
alec resnick <aresnickety@gmail.com>
2015-03-04 01:27:12 -0500

    Adding custom frequencies for cells, changing Board to initialize itself, and making styling for valid/invalid consistent after reset

--- script.js
+++ script.js
@@ -2,14 +2,5 @@ var board;
 window.addEventListener('load', function() { // When everything is loaded
     board = new Board([]); // Create an empty board
 
-    var cells = [];
-    for (var i = 0; i < 5*5; i++) {
-        var cell = new Cell();
-        cell.letter = String.fromCharCode(Math.random()*26 + 65);
-        cells.push(cell);
-    }; // Make an array of 5*5 = 25 new Cell objects
-
-    board.cells = cells; // and load the cells into the board
-
     document.body.appendChild(board.html); // Then actually put the Board's HTML into the document
 });
